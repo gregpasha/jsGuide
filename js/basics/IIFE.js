cbd9975cc1b16791092f2507e3ad0b17c3340802
Greg Bailey <gbailey@advent.com>
2014-06-03 20:45:02 -0700

    basics

---
+++ js/basics/IIFE.js
@@ -0,0 +1,31 @@
+define(function(require) {
+
+	var IIFE = function() {
+
+		// All code in this project is wrapped in an IIFE (Immediately Invoked Function Expression) 
+		// An IIFE creates a function scope and then gets executed, I am using this technique so you can put breakpoints 
+		// and watch all of the code in the project execute
+		(function(){
+
+		}());
+
+		// You can also put the bottom parens that invoke the function on the outside of IIFEs
+		// I think this is easier to read and prefer this syntax
+		(function(){
+
+		})();
+
+		// You can pass variables into IIFEs, here we are passing "this", which gets assigned to "global"
+		(function (global) { 
+			var something = global.something;
+		})(this);
+
+	}
+
+	return IIFE;
+});
+
+
+
+
+	
---
+++ js/basics/core.js
@@ -0,0 +1,56 @@
+define(function(require) {
+	'use strict'
+
+	var core = function() {
+
+		// warning - give variables good names, mine are terrible in this sample module because it doesn't do anything and ... lazy
+		(function(){
+
+			// javascript is written in Unicode and is case senstive			
+			var notEqual = ("x" !== "X"); // true;
+
+			// javascript ignores whitespace
+
+			
+			// this is a semi-colon, put one at the end of every line
+			// if you don't, javascript will do it automatically
+			// wars have been started over less then ASI (Automatic Semicolon Insertion), personally, I put them in.
+			;
+
+			// all numbers - integers and floats are both represented as 64 bit double floating point values
+			var a = 7;
+			var b = 77.45;
+
+			// strings, you can use single quotes or double quotes, pick one and stick with it
+			var c = 'this is b';
+			var d = "this is e";
+
+			// months use a 0 based index, so this is January 14, 2014
+			var e = new Date(2014, 0, 14);
+
+			var f = true;
+			var g = false;
+
+			// use = for variable assignment, === for comparison
+			// == does a type coercion and then a comparison, avoid.
+			var h = (f === g);
+
+			// ternary operator, a handy shortcut for if else
+			// this means if h is true then i = f, otherwise i = g;
+			var i = (h === true) ? f : g;
+
+			// array literal
+			var arr1 = []; 
+			var arr2 = [1, 2, 3];
+
+			// object
+			var obj1 = {}; 
+			var obj2 = {x: 36, y: 77};
+
+		})();
+
+	}
+	
+	return core;
+});
+
